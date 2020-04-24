7279888994f008d216f711e3fe81e0f2c8df958a
ocph23@gmail.com <ocph23@gmail.com>
2020-04-24 13:02:51 +0900

    as

--- client/js/routers/account.router.js
+++ client/js/routers/account.router.js
@@ -3,12 +3,12 @@ angular.module('account.router', [ 'ui.router' ]).config(function($stateProvider
 	$stateProvider
 		.state('account', {
 			url: '/account',
-			templateUrl: './client/views/account/index.html'
+			templateUrl: '/client/views/account/index.html'
 		})
 		.state('login', {
 			url: '/login',
 			parent: 'account',
 			controller: 'LoginController',
-			templateUrl: './client/views/account/sign-in.html'
+			templateUrl: '/client/views/account/sign-in.html'
 		});
 });
