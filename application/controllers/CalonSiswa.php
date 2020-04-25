3639d568362abdb61a38e9a1ec975bcb0b38ac38
kristt26 <kristt26@gmail.com>
2020-04-25 23:54:59 +0900

    Simpan Siswa

--- application/controllers/CalonSiswa.php
+++ application/controllers/CalonSiswa.php
@@ -14,24 +14,18 @@ class CalonSiswa extends \Restserver\Libraries\REST_Controller
 
     public function GetSiswa_get()
     {
-        $output = $this->CalonSiswa_model->select(isset($_GET)? $_GET['idcalonsiswa']: false);
+        $output = $this->CalonSiswa_model->select(isset($_GET) ? $_GET['idcalonsiswa'] : false);
         $this->response($output, REST_Controller::HTTP_OK);
 
     }
     public function simpan_post()
     {
-        $this->load->library('Authorization_Token');
-        $is_valid_token = $this->authorization_token->validateToken();
-        if ($is_valid_token['status'] === true) {
-            $POST = json_decode($this->security->xss_clean($this->input->raw_input_stream), true);
-            $Output = $this->CalonSiswa_model->insert($POST);
-            if ($Output) {
-                $this->response($Output, REST_Controller::HTTP_OK);
-            } else {
-                $this->response(false, REST_Controller::HTTP_BAD_REQUEST);
-            }
+        $POST = json_decode($this->security->xss_clean($this->input->raw_input_stream), true);
+        $Output = $this->CalonSiswa_model->insert($POST);
+        if ($Output) {
+            $this->response($Output, REST_Controller::HTTP_OK);
         } else {
-            $this->response(null, REST_Controller::HTTP_UNAUTHORIZED);
+            $this->response(false, REST_Controller::HTTP_BAD_REQUEST);
         }
     }
     public function ubah_put()
