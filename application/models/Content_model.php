5acaddac6174e4b87c9c3a01e39ce559edbe3e09
kristt26 <kristt26@gmail.com>
2020-04-28 19:09:28 +0900

    Content

--- application/models/Content_model.php
+++ application/models/Content_model.php
@@ -22,38 +22,46 @@ class Content_Model extends CI_Model
 
     public function insert($data)
     {
+        $this->load->library('Exceptions');
         $this->db->trans_begin();
-        $data = (object) $data;
-        $item = [
-            'content' => $data->content,
-            'status' => $value->status ? 1 : 0
-        ];
-        $this->db->insert('content', $item);
-        $data->idcontent = $this->db->insert_id();
-        if ($this->db->trans_status() === false) {
-            $this->db->trans_rollback();
-            return false;
-        } else {
-            $this->db->trans_commit();
+        $data = $data;
+        try {
+            $item = [
+                'content' => $data->content,
+                'publish' => $data->publish ? 1 : 0,
+                'created' => $data->created,
+                'title' => $data->title,
+                'type' => $data->type,
+            ];
+            $this->db->insert('content', $item);
+            $this->exceptions->checkForError();
+            $data['idcontent'] = $this->db->insert_id();
             return $data;
+        } catch (IMySQLException $th) {
+            
         }
     }
     public function update($data)
     {
+        $this->load->library('Exceptions');
         $this->db->trans_begin();
         $data = (object) $data;
-        $item = [
-            'content' => $data->content,
-            'status' => $value->status ? 1 : 0
-        ];
-        $this->db->where('idcontent', $data->idcontent);
-        $this->db->update('content', $item);
-        if ($this->db->trans_status() === false) {
+        try {
+            $item = [
+                'content' => $data->content,
+                'publish' => $data->publish ? 1 : 0,
+                'created' => $data->created,
+                'title' => $data->title,
+                'type' => $data->type,
+            ];
+            $this->db->where('idcontent', $data->idcontent);
+            $this->db->update('content', $item);
+            return $data;
+        } catch (\Throwable $th) {
             $this->db->trans_rollback();
+            $model = $th->getErrorMessage();
+            throw new Exception($model['error']['message']);
             return false;
-        } else {
-            $this->db->trans_commit();
-            return true;
         }
     }
     public function delete($id)
