ce1e1b95fa764186551cfc3f8a5b1d67c1fd71e8
ocph23@gmail.com <ocph23@gmail.com>
2020-05-12 23:47:57 +0900

    fix

--- client/js/controllers/siswa.controllers.js
+++ client/js/controllers/siswa.controllers.js
@@ -15,21 +15,43 @@ function siswaController($scope, $state, AuthService, StorageService) {
 
 function siswaHomeController($scope, AuthService, CalonSiswaService) {}
 
-function siswaProfileController($scope, AuthService, CalonSiswaService) {
-	if (AuthService.userIsLogin()) {
-		AuthService.profile().then((profile) => {
-			CalonSiswaService.getById(profile.biodata.idcalonsiswa).then((data) => {
-				$scope.model = data;
-				console.log(data);
+function siswaProfileController($scope, AuthService, CalonSiswaService, TahunAjaranService) {
+	AuthService.profile().then((profile) => {
+		CalonSiswaService.getById(profile.biodata.idcalonsiswa).then((data) => {
+			$scope.model = data;
+			TahunAjaranService.get().then((result) => {
+				$scope.taActive = result.find((x) => x.status);
 			});
 		});
-	}
-	$scope.print=()=>{
+	});
+
+	$scope.print = () => {
 		setTimeout(() => {
 			window.print();
 		}, 3000);
-		
-	}
+	};
 }
 
-function siswaPengumumanController($scope) {}
+function siswaPengumumanController($scope, AuthService, TahunAjaranService) {
+	$scope.pengumuman = false;
+	TahunAjaranService.get().then((ta) => {
+		var taActive = ta.find((x) => x.status);
+		if (taActive.pengumuman) {
+			$scope.pengumuman = true;
+		} else {
+			$scope.belumAdaPengumuman = true;
+		}
+
+		AuthService.profile().then((profile) => {
+			if (profile.biodata.status) {
+				$scope.pengumuman = true;
+				if (profile.biodata.status == '1') {
+					profile.biodata.status = 'Lulus';
+				} else {
+					profile.biodata.status = 'Tidak Lulus';
+				}
+			}
+			$scope.model = profile;
+		});
+	});
+}
